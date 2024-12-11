0becf74d827c794588d1048d8586f03fbdbc955d
Egor Yaroshko <ego8973@gmail.com>
2024-12-11 09:05:05 +0300

    Update uni.sh

--- uni.sh
+++ uni.sh
@@ -26,3 +26,4 @@ docker-compose up -d
 # Проверяем ноду с помощью curl
 curl -d '{"id":1,"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest",false]}' \
   -H "Content-Type: application/json" http://localhost:8545
+  
